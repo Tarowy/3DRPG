b6842afc3f2807363ee16f711c1329cb9db50672
Tarowy <72642270+Tarowy@users.noreply.github.com>
2022-02-25 20:52:10 +0800

    添加人物动画和人物防遮挡

--- Assets/Scripts/Character/PlayerManger.cs
+++ Assets/Scripts/Character/PlayerManger.cs
@@ -7,15 +7,28 @@ using UnityEngine.AI;
 public class PlayerManger : MonoBehaviour
 {
     private NavMeshAgent _navMeshAgent;
+    private Animator _animator;
 
     private void Awake()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
+        _animator = GetComponent<Animator>();
     }
 
     private void Start()
     {
         MouseManager.Instance._onMouseClicked += MoveToTarget;
+    }
+
+    private void Update()
+    {
+        SwitchAnimation();
+    }
+
+    private void SwitchAnimation()
+    {
+        _animator.SetFloat("speed",_navMeshAgent.velocity.sqrMagnitude); //将速度矢量转换为浮点数传入speed
+        Debug.Log("speed:" + _navMeshAgent.velocity.sqrMagnitude);
     }
 
     private void MoveToTarget(Vector3 target)
